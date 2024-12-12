fa6a2b0c5f6baddb70878dbfe0756faeebeb7b7c
wadia20 <wadiasouiki@gmail.com>
2024-12-12 21:25:23 +0100

    commit

--- src/main/java/com/example/devoirthreads/OrderProcessor.java
+++ src/main/java/com/example/devoirthreads/OrderProcessor.java
@@ -25,7 +25,7 @@ public class OrderProcessor extends Thread {
             try {
                 // Read the orders.json file
                 ObjectMapper mapper = new ObjectMapper();
-                ObjectNode rootNode = (ObjectNode) mapper.readTree(new File("C:\\Users\\ahmed\\IdeaProjects\\devoirThreqds\\Folders\\input.json"));
+                ObjectNode rootNode = (ObjectNode) mapper.readTree(new File("C:\\Users\\ahmed\\IdeaProjects\\devoirThreqds\\Files\\input.json"));
                 ArrayNode ordersArray = (ArrayNode) rootNode.get("orders");
 
                 // Prepare output lists
@@ -53,11 +53,11 @@ public class OrderProcessor extends Thread {
                 // Create output files
                 ObjectNode outputRoot = mapper.createObjectNode();
                 outputRoot.set("orders", mapper.valueToTree(validOrders));
-                mapper.writerWithDefaultPrettyPrinter().writeValue(new File("C:\\Users\\ahmed\\IdeaProjects\\devoirThreqds\\Folders\\output.json"), outputRoot);
+                mapper.writerWithDefaultPrettyPrinter().writeValue(new File("C:\\Users\\ahmed\\IdeaProjects\\devoirThreqds\\Files\\output.json"), outputRoot);
 
                 ObjectNode errorRoot = mapper.createObjectNode();
                 errorRoot.set("orders", mapper.valueToTree(invalidOrders));
-                mapper.writerWithDefaultPrettyPrinter().writeValue(new File("C:\\Users\\ahmed\\IdeaProjects\\devoirThreqds\\Folders\\error.json"), errorRoot);
+                mapper.writerWithDefaultPrettyPrinter().writeValue(new File("C:\\Users\\ahmed\\IdeaProjects\\devoirThreqds\\Files\\error.json"), errorRoot);
                 for (ObjectNode i:validOrders){
                     System.out.println(i);
                 }
